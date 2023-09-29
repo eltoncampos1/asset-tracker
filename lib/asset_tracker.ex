f0f02eea8f0e9a1a14a7ef1de138b5f4938736c8
Elton Campos <eltoncampos36@gmail.com>
2023-09-28 21:31:35 -0300

    feat: setup elixir app

---
+++ lib/asset_tracker.ex
@@ -0,0 +1,18 @@
+defmodule AssetTracker do
+  @moduledoc """
+  Documentation for `AssetTracker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AssetTracker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
