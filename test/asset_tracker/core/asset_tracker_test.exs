8c072fe26d1592f20582cf8b6ec9984a9f9818ab
Elton Campos <eltoncampos36@gmail.com>
2023-09-28 22:32:52 -0300

    test: create a new empty asset tracker

---
+++ test/asset_tracker/core/asset_tracker_test.exs
@@ -0,0 +1,9 @@
+defmodule AssetTracker.Core.AssetTrackerTest do
+  use ExUnit.Case
+
+  alias AssetTracker.Core.AssetTracker
+
+  test "new/0" do
+    assert %AssetTracker{purchases: [], sales: []} = AssetTracker.new()
+  end
+end
--- test/asset_tracker_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AssetTrackerTest do
-  use ExUnit.Case
-  doctest AssetTracker
-
-  test "greets the world" do
-    assert AssetTracker.hello() == :world
-  end
-end
