8eb2dddc6a64f4d190491cf86150f56bac3d3d10
Elton Campos <eltoncampos36@gmail.com>
2023-10-02 00:36:10 -0300

    feat: docs to repository

--- lib/asset_tracker/core/Errors.ex
+++ lib/asset_tracker/core/Errors.ex
@@ -1,3 +1,6 @@
 defmodule AssetTracker.Core.Errors do
+  @moduledoc """
+  Centralize commom errors
+  """
   def not_found, do: {:error, :not_found}
 end
--- lib/asset_tracker/ports/repository.ex
+++ lib/asset_tracker/ports/repository.ex
@@ -1,4 +1,7 @@
 defmodule AssetTracker.Ports.Repository do
+  @moduledoc """
+  Interface for interacting with Repository Providers
+  """
   @adapter Application.compile_env!(:asset_tracker, [__MODULE__, :adapter])
 
   @type return :: {:ok, term()} | {:error, term()}
@@ -6,13 +9,39 @@ defmodule AssetTracker.Ports.Repository do
   @callback insert(table :: atom(), params :: any()) :: return()
   @callback update(table :: atom(), id :: binary(), params :: any()) :: return()
   @callback get(table :: atom(), id :: binary()) :: return()
+  @doc """
+  Insert a new record
 
+  ## Examples
+
+      iex> AssetTracker.Ports.Repository.insert(table, params)
+      {:ok, params}
+
+  """
   @spec insert(table :: atom(), params :: any()) :: return()
   defdelegate insert(table, params), to: @adapter
 
+  @doc """
+  Update a existent record
+
+  ## Examples
+
+      iex> AssetTracker.Ports.Repository.update(table, id, new_params)
+      {:ok, new_params}
+
+  """
   @spec update(table :: atom(), id :: binary(), params :: any()) :: return()
   defdelegate update(table, id, params), to: @adapter
 
+  @doc """
+  get existent record
+
+  ## Examples
+
+      iex> AssetTracker.Ports.Repository.get(table, id)
+      {:ok, record}
+
+  """
   @spec get(table :: atom(), id :: binary()) :: return()
   defdelegate get(table, id), to: @adapter
 end
